da813e0f06353474da3757b48bb9768ef4909f33
t1433 <gcdw778gsz@privaterelay.appleid.com>
2025-12-31 23:57:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-// レベル表示だけ
-const level = document.getElementById("level");
-const levelValue = document.getElementById("levelValue");
+const levelNumber = document.getElementById("levelNumber");
+const levelRange = document.getElementById("levelRange");
 
-level.addEventListener("input", () => {
-  levelValue.textContent = level.value;
+// 数値 → バー
+levelNumber.addEventListener("input", () => {
+  let value = Number(levelNumber.value);
+
+  if (value < 1) value = 1;
+  if (value > 100) value = 100;
+
+  levelNumber.value = value;
+  levelRange.value = value;
+});
+
+// バー → 数値
+levelRange.addEventListener("input", () => {
+  levelNumber.value = levelRange.value;
 });
