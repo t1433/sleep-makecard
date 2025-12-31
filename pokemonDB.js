ea8037a5178e4e958d2698e336f7e919aa559502
t1433 <gcdw778gsz@privaterelay.appleid.com>
2026-01-01 01:57:58 +0900

    Update pokemonDB.js

--- pokemonDB.js
+++ pokemonDB.js
@@ -1,1 +1,35 @@
+const pokemonDB = {
+  "ピカチュウ": {
+    type: "でんき",
+    mainSkill: "げんきチャージS",
+    baseCarry: 15,
+    ingredients: {
+      A: "とくせんリンゴ",
+      B: "モーモーミルク",
+      C: null
+    }
+  },
+
+  "イーブイ": {
+    type: "ノーマル",
+    mainSkill: "エナジーチャージS",
+    baseCarry: 18,
+    ingredients: {
+      A: "モーモーミルク",
+      B: "あまいミツ",
+      C: null
+    }
+  },
+
+  "カビゴン": {
+    type: "ノーマル",
+    mainSkill: "エナジーチャージM",
+    baseCarry: 28,
+    ingredients: {
+      A: "あまいミツ",
+      B: "マメミート",
+      C: "ワカクサ大豆"
+    }
+  }
+};
 
