97c2e18849e9750df6aaef68188ac0e58f7ef112
t1433 <gcdw778gsz@privaterelay.appleid.com>
2026-01-01 00:01:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const levelNumber = document.getElementById("levelNumber");
 const levelRange = document.getElementById("levelRange");
 
-// 数値 → バー
 levelNumber.addEventListener("input", () => {
   let value = Number(levelNumber.value);
 
@@ -12,7 +11,29 @@ levelNumber.addEventListener("input", () => {
   levelRange.value = value;
 });
 
-// バー → 数値
 levelRange.addEventListener("input", () => {
   levelNumber.value = levelRange.value;
 });
+
+const sleepHourInput = document.getElementById("sleepHour");
+const sleepMinuteInput = document.getElementById("sleepMinute");
+const maxCarryBox = document.getElementById("maxCarry");
+
+sleepHourInput.addEventListener("input", updateCarryFromSleep);
+sleepMinuteInput.addEventListener("input", updateCarryFromSleep);
+
+function updateCarryFromSleep() {
+  const hours = Number(sleepHourInput.value) || 0;
+  const minutes = Number(sleepMinuteInput.value) || 0;
+
+  const totalHours = hours + minutes / 60;
+
+  let bonus = 0;
+
+  if (totalHours >= 200) bonus += 1;
+  if (totalHours >= 500) bonus += 2;
+  if (totalHours >= 1000) bonus += 3;
+  if (totalHours >= 2000) bonus += 2;
+
+  maxCarryBox.textContent = `睡眠ボーナス +${bonus} 個`;
+}
