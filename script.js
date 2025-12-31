589b72cb8f0a9581522c346aeb2b55d6d8e6ce46
t1433 <gcdw778gsz@privaterelay.appleid.com>
2025-12-31 23:55:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// レベル表示だけ
+const level = document.getElementById("level");
+const levelValue = document.getElementById("levelValue");
 
+level.addEventListener("input", () => {
+  levelValue.textContent = level.value;
+});
